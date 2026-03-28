b5588b182fd5a03a932baf3a2e27d3fe48230cf5
ugulavaleri <leri.ugulava1@gmail.com>
2026-03-28 21:40:56 +0400

    change port

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/weather/:path*',
-        destination: 'http://localhost:8000/api/weather/:path*',
+        destination: 'http://localhost:5000/api/weather/:path*',
       },
     ];
   },
